ce43109cf031ef2560e6f69a47fe6c3487790914
ngetich kimutai daniel <daniel.ngetich@student.moringaschool.com>
2023-06-03 21:10:09 +0300

    added extension officer

---
+++ db/migrate/20230603180919_create_extension_officers.rb
@@ -0,0 +1,13 @@
+class CreateExtensionOfficers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :extension_officers do |t|
+      t.string :name
+      t.string :email
+      t.integer :telephone
+      t.string :county
+      t.string :sub-county
+
+      t.timestamps
+    end
+  end
+end
